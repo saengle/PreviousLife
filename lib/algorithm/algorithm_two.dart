65e62e2f5ee3fa61888885dd5427a258917e5f4a
saengle <zohv817@gmail.com>
2022-09-21 16:25:06 +0900

    Add: 알고리즘 문제 하나 추가

--- lib/algorithm/algorithm_two.dart
+++ lib/algorithm/algorithm_two.dart
@@ -1,19 +1,51 @@
-void main() {
-  /* 알고리즘 문제풀이  N, M 두 숫자가 짝수와 홀수의 쌍이면 "YES", 그렇지 않으면 "NO"를 출력합니다.
+// void main() {
+//   /* 알고리즘 문제풀이  N, M 두 숫자가 짝수와 홀수의 쌍이면 "YES", 그렇지 않으면 "NO"를 출력합니다.
+//
+// 끝에 줄 바꿈을 넣고 불필요한 문자나 빈 행을 포함하지 마십시오 */
+//   print(solution(13, 25));
+//   // No
+//   print(solution(24, 12));
+//   // No
+//   print(solution(13, 24));
+//   // Yes
+// }
+//
+// String solution(int n, int m) {
+//   if ((n + m) % 2 == 0) {
+//     return 'NO';
+//   } else {
+//     return 'YES';
+//   }
+// }
 
-끝에 줄 바꿈을 넣고 불필요한 문자나 빈 행을 포함하지 마십시오 */
-  print(solution(13, 25));
-  // No
-  print(solution(24, 12));
-  // No
-  print(solution(13, 24));
-  // Yes
+
+
+void main() {
+  /*
+  등차수열 m과 n 을 입력받아 m 이 n 만큼 10개까지 늘어나는 수열을 만드시오.
+   */
+  print(solution(3, 3)); // 3 6 9 12 15 18 21 24 27 30
+
+  print(solution(5, 10)); // 5 15 25 35 45 55 65 75 85 95
+
+  print(solution(1, 3)); // 1 4 7 10 13 16 19 22 25 28
 }
 
-String solution(int n, int m) {
-  if ((n + m) % 2 == 0) {
-    return 'NO';
-  } else {
-    return 'YES';
+
+String solution(int m, int n) {
+  List<int> result = [];
+  for (int i = 0; i < 10; i++) {
+    result.add(m + (n * i));
   }
+
+  String realResult = '';
+  for (int i = 0; i < result.length; i++) {
+    if (i == result.length - 1) {
+      realResult += result[i].toString();
+    } else {
+      realResult += result[i].toString() + ' ';  //  += '${result[i]} '
+    }
+  }
+
+  return realResult;
 }

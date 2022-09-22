430ec243c196f9576ca7d22a6481cb8bba85afa1
saengle <zohv817@gmail.com>
2022-09-22 17:11:26 +0900

    Add: 알고리즘 문제풀이 추가

--- lib/algorithm/algorithm_two.dart
+++ lib/algorithm/algorithm_two.dart
@@ -18,34 +18,48 @@
 //   }
 // }
 
-
-
+//
+//
+// void main() {
+//   /*
+//   등차수열 m과 n 을 입력받아 m 이 n 만큼 10개까지 늘어나는 수열을 만드시오.
+//    */
+//   print(solution(3, 3)); // 3 6 9 12 15 18 21 24 27 30
+//
+//   print(solution(5, 10)); // 5 15 25 35 45 55 65 75 85 95
+//
+//   print(solution(1, 3)); // 1 4 7 10 13 16 19 22 25 28
+// }
+//
+//
+// String solution(int m, int n) {
+//   List<int> result = [];
+//   for (int i = 0; i < 10; i++) {
+//     result.add(m + (n * i));
+//   }
+//
+//   String realResult = '';
+//   for (int i = 0; i < result.length; i++) {
+//     if (i == result.length - 1) {
+//       realResult += result[i].toString();
+//     } else {
+//       realResult += result[i].toString() + ' ';  //  += '${result[i]} '
+//     }
+//   }
+//
+//   return realResult;
+// }
+// //
+//
+// String solution(int c_1, int c_2, int c_3, int c_4) {
+//   int result = 15 - (c_1 + c_2 + c_3 + c_4);
+//   return result.toString();
+// }
 void main() {
-  /*
-  등차수열 m과 n 을 입력받아 m 이 n 만큼 10개까지 늘어나는 수열을 만드시오.
-   */
-  print(solution(3, 3)); // 3 6 9 12 15 18 21 24 27 30
-
-  print(solution(5, 10)); // 5 15 25 35 45 55 65 75 85 95
-
-  print(solution(1, 3)); // 1 4 7 10 13 16 19 22 25 28
+  print(solution('4444'));
+  print(solution('3353'));
 }
 
-
-String solution(int m, int n) {
-  List<int> result = [];
-  for (int i = 0; i < 10; i++) {
-    result.add(m + (n * i));
-  }
-
-  String realResult = '';
-  for (int i = 0; i < result.length; i++) {
-    if (i == result.length - 1) {
-      realResult += result[i].toString();
-    } else {
-      realResult += result[i].toString() + ' ';  //  += '${result[i]} '
-    }
-  }
-
-  return realResult;
+String solution(String n) {
+  return n.split('').toSet().length == 1 ? n : 'No';
 }
